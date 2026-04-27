c0c0a6483da86b5a3d6c15d0eae82826e0642e78
kysilkavojtech <kysilkavojtech@gmail.com>
2026-04-27 19:17:54 -0400

    Extra security settings - ddos, https cookies, xss

--- backend/settings.py
+++ backend/settings.py
@@ -136,4 +136,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+    },
 }
+
+if not DEBUG:
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
